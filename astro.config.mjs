e00ff2cfd26ccb10edf0ec7cc2158c8799a25a8e
Afif Alfiano <m.afif@cakap.id>
2024-07-10 08:30:25 +0700

    feat(vercel): add vercel for build static

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
-
+import vercelStatic from '@astrojs/vercel/static';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
+	output: 'static',
+  adapter: vercelStatic(),
 });
