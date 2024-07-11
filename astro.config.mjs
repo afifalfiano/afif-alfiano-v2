dc8dea1ee1e22681de55614cd08a0fe9bb8d360f
Afif Alfiano <m.afif@cakap.id>
2024-07-11 11:27:03 +0700

    feat(partytown): use lib to forward tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from "astro/config";
 import vercelStatic from '@astrojs/vercel/static';
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind()],
+	integrations: [
+		tailwind(),
+		partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+	],
 	output: 'static',
   adapter: vercelStatic(),
 });
