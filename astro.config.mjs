44d35cf20edaa289d159b9824b44d706f8d3fd25
Afif Alfiano <m.afif@cakap.id>
2024-07-12 07:24:01 +0700

    feat: add seo config and opengraph generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,25 @@ import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import sitemap from '@astrojs/sitemap';
 import vercel from '@astrojs/vercel/static';
+import opengraphImages, { presets } from "astro-opengraph-images";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		tailwind(),
+    opengraphImages({
+      options: {
+        fonts: [
+          {
+            name: "Roboto",
+            weight: 400,
+            style: "normal",
+            data: fs.readFileSync("node_modules/@fontsource/roboto/files/roboto-latin-400-normal.woff"),
+          },
+        ],
+      },
+      render: presets.tailwind,
+    }),
 		partytown({
       // Adds dataLayer.push as a forwarding-event.
       config: {
